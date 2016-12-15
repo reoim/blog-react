67c1906c945688834fa462867106966821c2e583
Dongmin (Reo) Lee <reoim10@gmail.com>
2016-12-15 23:53:22 +1100

    React Form validation

--- src/components/posts_new.js
+++ src/components/posts_new.js
@@ -1,42 +1,78 @@
 import React, { Component } from 'react';
 import { reduxForm } from 'redux-form';
+import { createPost } from '../actions/index';
+import { Link } from 'react-router';
+
 
 class PostsNew extends Component {
 	render(){
 
 		const { fields: { title, categories, content }, handleSubmit } = this.props;
-		// same as 
+		// same as
 		// const handleSubmit = this.props. handleSubmit
 		// const title = this.props.fields.title
 		// const categories = this.props.fields.categories
 		// const content = this.props.fields.content
 
 		return(
-			<form onSubmit={handleSubmit}>
-				<h3>Create A New Post</h3>
-				<div className="form-group">
+			<form onSubmit={handleSubmit(this.props.createPost)}>
+				<h3>Create a New Post</h3>
+				<div className={`form-group ${title.touched && title.invalid ? 'has-danger' : ''}`}>
 					<label>Title</label>
 					<input type="text" className="form-control" {...title} />
+					<div className="text-help">
+						{title.touched ? title.error : ''}
+					</div>
 				</div>
-				<div className="form-group">
+				<div className={`form-group ${categories.touched && categories.invalid ? 'has-danger' : ''}`}>
 					<label>Categories</label>
-					<input type="text" className="form-control" />
+					<input type="text" className="form-control" {...categories} />
+					<div className="text-help">
+						{categories.touched ? categories.error : ''}
+					</div>
 				</div>
-				<div className="form-group">
+				<div className={`form-group ${content.touched && content.invalid ? 'has-danger' : ''}`}>
 					<label>Content</label>
-					<input type="text" className="form-control" />
+					<textarea className="form-control" {...content} />
+					<div className="text-help">
+						{content.touched ? content.error : ''}
+					</div>
 				</div>
 
 				<button type="submit" className="btn btn-primary">Submit</button>
+				<Link to="/" className="btn btn-danger">Cancel</Link>
 			</form>
 		);
 	}
 }
 
+function validate(values) {
+	const errors = {};
+
+	if (!values.title) {
+		errors.title = 'Enter a username';
+	}
+
+	if (!values.categories) {
+		errors.categories = 'Enter a category';
+	}
+
+	if (!values.content) {
+		errors.content = 'Enter some content';
+	}
+
+
+	return errors;
+}
+
+// connect: first argument is mapStateToProps, 2nd is mapDispatchToProps
+// reduxForm: 1st is form config, 2nd is mapStateToProps, 3rd is mapDispatchToProps
+
 export default reduxForm({
 	// name of form does not need to be match with component name
-	// but it need to be unique. 
+	// but it need to be unique.
 	// Especially do not use same name as formReducer
-	form: 'PostsNewForm', 
-	fields: ['title', 'categories', 'content']
-})(PostsNew);
+	form: 'PostsNewForm',
+	fields: ['title', 'categories', 'content'],
+	validate
+}, null, { createPost })(PostsNew);
